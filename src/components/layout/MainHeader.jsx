cf0df6c490a6db60299a8537fefab01f22a9019f
olilu <o.m.luetolf@gmail.com>
2023-09-28 17:23:25 +0200

    use navigate instead of link

--- src/components/layout/MainHeader.jsx
+++ src/components/layout/MainHeader.jsx
@@ -1,10 +1,15 @@
-import { Link } from 'react-router-dom';
-import { Nav, Navbar, Container } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { Nav, Navbar, Container, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import logo from '../../assets/bargain_hand_white.png';
 
 function MainHeader() {
+  const navigate = useNavigate();
+  function addWishlistHandler() {
+    navigate('/add-wishlist')
+  }
+
   return (
     <Navbar expand="lg" bg="dark" data-bs-theme="dark">
       <Container>
@@ -13,12 +18,12 @@ function MainHeader() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} href="/">Home</Nav.Link>
-            <Nav.Link href="/add-wishlist">Add Wishlist</Nav.Link>
+            <Nav.Item className="ms-2"><Button variant='light' onClick={addWishlistHandler}>Add Wishlist</Button></Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
 export default MainHeader;
